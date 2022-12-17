c8327ace85a333c3661a5376cd62aeaa0d4925aa
GetialJuan <getial.juan@correounivalle.edu.co>
2022-12-17 18:00:56 -0500

    Actividad 2 lista

--- actividad2/scripts/queries.sql
+++ actividad2/scripts/queries.sql
@@ -6,22 +6,58 @@ SELECT COUNT(river) AS numero_rios, name
     ) AS river_country JOIN country ON river_country.country = country.code
     GROUP BY name HAVING COUNT(river) > 15;
 
+    -- Algebra Relacional
+    /*
+    σ numero_rios > 15 
+	    (γ name; count(river) → numero_rios 
+		    ((π river, country (geo_river)) 
+			    ⨝ country = country.code (country)))
+    */
+
 -- Ejercicio No. 2. Obtenga el nombre de los países que pertenecen o son miembros de la UNESCO o de UNASUR.
 SELECT name FROM (
     SELECT country FROM ismember WHERE organization = 'UNESCO' OR organization = 'UNASUR'
 ) AS country_unesco_unasur JOIN country ON country_unesco_unasur.country = country.code;
 
+    -- Algebra Relacional
+    /*
+    π name 
+	    ((σ organization = 'UNESCO' ∨ organization = 'UNASUR' (ismember))
+		    ⨝ country = country.code (country))
+    */
+
 -- Ejercicio No. 3: ¿Cuál es el nombre, población y altitud de cada ciudad en Colombia (según lo registrado en
 -- la base de datos)? Los resultados deben estar ordenados de manera descendente por población.
-SELECT citypops.city, citypops.population, elevation FROM (
-    SELECT city, MAX(year) AS año_mas_reciente FROM citypops WHERE  country = 'CO' GROUP BY city
-) AS max_year_city 
-    JOIN citypops ON max_year_city.año_mas_reciente = citypops.year 
-        AND max_year_city.city = citypops.city AND citypops.country = 'CO'
-    JOIN city ON citypops.city = city.name AND city.country = 'CO'
-    ORDER BY population DESC;
+SELECT name, population, elevation FROM city WHERE country = 'CO' ORDER BY population DESC;
+
+    -- Algebra Relacional
+    /*
+    τ population desc (
+	    π name, population, elevation (
+		    σ country = 'CO' (city)
+	    )
+    )
+    */
 
 -- Ejercicio No. 4: Obtenga los códigos de todos los países para los cuales (a) el PIB está compuesto al menos
 -- en un 70% por los sectores de Servicios e Industria juntos, o (b) la inflación es inferior al 2%.
 SELECT country FROM economy 
 WHERE service + industry >= 70 OR inflation < 2;
+
+    -- Algebra Relacional
+    /*
+    π country (
+	    σ service + industry >= 70 ∨ inflation < 2 (economy)
+    )
+    */
+
+
+--## (b) el número de registros en la respuesta ##
+-- Para saber el numero de registros en cada respuesta solo se debe reemplazar la consulta("querie") realizada
+-- segun el caso:
+
+    -- SQL
+        --- SELECT COUNT(*) FROM (querie);
+
+    -- Algebra relacional
+        -- γ count(*) → numero_de_registros (querie)
