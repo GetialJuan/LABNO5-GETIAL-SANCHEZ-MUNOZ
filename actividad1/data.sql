dcfc605f88ad238670ea0c95686e8002f6d9936e
ITS - Mauricio Munoz <mauricio.munoz@its.com.co>
2022-12-23 19:32:24 -0500

    corregido 1.2

--- actividad1/data.sql
+++ actividad1/data.sql
@@ -3,14 +3,14 @@ INSERT INTO tabla1(A,B) VALUES
     ('c','d'),
     ('e','f');
     
-INSERT INTO tabla2(A,C,D) VALUES
-	('a','x',1),
-    ('c','y',2),
-    ('e','z',3),
-    ('a','y',1);
-    
 INSERT INTO tabla3(C,D,E) VALUES
 	('x',1,'r'),
     ('y',1,'s'),
     ('z',3,'t'),
     ('y',2,'p');
+    
+INSERT INTO tabla2(A,C,D) VALUES
+	('a','x',1),
+    ('c','y',2),
+    ('e','z',3),
+    ('a','y',1);
